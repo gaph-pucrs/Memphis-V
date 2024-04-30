280ca76b86a3040cb8459e6bfbd343c3c94134e1
Angelo Elias Dal Zotto <angelodalzotto97@gmail.com>
2024-04-30 17:47:16 -0300

    Add ram and cpu dbug

--- modules/scenario.py
+++ modules/scenario.py
@@ -68,6 +68,8 @@ class Scenario:
 
 		makedirs(self.base_dir+"/debug/pipe", exist_ok=True)
 		makedirs(self.base_dir+"/debug/request", exist_ok=True)
+		makedirs(self.base_dir+"/debug/ram", exist_ok=True)
+		makedirs(self.base_dir+"/debug/cpu", exist_ok=True)
 		makedirs(self.base_dir+"/log", exist_ok=True)
 		makedirs(self.base_dir+"/flit_sniffer", exist_ok=True)
 		makedirs(self.base_dir+"/management", exist_ok=True)
