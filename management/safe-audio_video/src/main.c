1d61cde1cc223975fb70461d283b400e15c70b92
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2024-09-30 10:52:19 -0300

    Use a less stupid form of computing prod/cons

--- management/safe-audio_video/src/main.c
+++ management/safe-audio_video/src/main.c
@@ -28,7 +28,8 @@ int main()
 {
 	printf("SAFE audio_video model started at %d\n", memphis_get_tick());
 
-	int num_inf = 0;
+	unsigned num_inf = 0;
+	unsigned time = 0;
 	while (true) {
 		static uint32_t msg[7];
 		memphis_receive_any(msg, sizeof(msg));
@@ -39,85 +40,34 @@ int main()
 				break;
 			case SEC_INFER:
 				num_inf++;
-				bool prod_0 = false;
-				bool prod_1 = false;
-				bool prod_2 = false;
-				bool prod_3 = false;
-				bool prod_4 = false;
-				bool prod_6 = false;
-				bool cons_0 = false;
-				bool cons_1 = false;
-				bool cons_2 = false;
-				bool cons_3 = false;
-				bool cons_4 = false;
-				bool cons_5 = false;
-				switch (msg[4]) {
-					case 0:
-						prod_0 = true;
-						break;
-					case 1:
-						prod_1 = true;
-						break;
-					case 2:
-						prod_2 = true;
-						break;
-					case 3:
-						prod_3 = true;
-						break;
-					case 4:
-						prod_4 = true;
-						break;
-					case 6:
-						prod_6 = true;
-						break;
-					default:
-						break;
-				}
-				switch (msg[5]) {
-					case 0:
-						cons_0 = true;
-						break;
-					case 1:
-						cons_1 = true;
-						break;
-					case 2:
-						cons_2 = true;
-						break;
-					case 3:
-						cons_3 = true;
-						break;
-					case 4:
-						cons_4 = true;
-						break;
-					case 5:
-						cons_5 = true;
-						break;
-					default:
-						break;
-				}
-				// unsigned then = memphis_get_tick();
+				bool prod[6] = {};
+				bool cons[6] = {};
+				prod[msg[4]] = true;
+				cons[msg[5]] = true;
+				unsigned then = memphis_get_tick();
 				float timestamp = msg[1] / 100000.0;
 				float real_latency = msg[6] / 100.0;
 				float pred_latency = score(
 					timestamp, 
 					msg[3], 
 					msg[2], 
-					prod_0, 
-					prod_1, 
-					prod_2, 
-					prod_3, 
-					prod_4, 
-					prod_6, 
-					cons_0, 
-					cons_1, 
-					cons_2, 
-					cons_3, 
-					cons_4, 
-					cons_5
+					prod[0], 
+					prod[1], 
+					prod[2], 
+					prod[3], 
+					prod[4], 
+					prod[6], 
+					cons[0], 
+					cons[1], 
+					cons[2], 
+					cons[3], 
+					cons[4], 
+					cons[5]
 				);
 				float diff = fabs(real_latency - pred_latency) / pred_latency;
 				bool anom = diff > 0.05;
-				// unsigned now = memphis_get_tick();
+				unsigned now = memphis_get_tick();
+				time += (now-then);
 				// printf("Inference in %u us\n", (now - then)/100);
 				// printf("Instance %f,%lu,%lu,%d,%d,%f,%f\n", timestamp, msg[3], msg[2], (int)msg[4], (int)msg[5], real_latency, pred_latency);
 				// printf("Diff %.2f%%\n", diff);
@@ -131,7 +81,7 @@ int main()
 				}
 				break;
 			case TERMINATE_ODA:
-				printf("Exiting after %d inferences\n", num_inf);
+				printf("Exiting after %d inferences that took ~%u ticks\n", num_inf, time/num_inf);
 				return 0;
 			default:
 				break;
