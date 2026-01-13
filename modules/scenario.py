2b5119fe1a95035c25257172dab6277a54116f3d
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2026-01-13 09:59:23 -0300

    Tune HT to start at 0 (filtered) and activation to 50-100 cycles

--- modules/scenario.py
+++ modules/scenario.py
@@ -190,9 +190,9 @@ class Scenario:
 		for link in self.links:
 			if link[1][0] == "rs":
 				with open("{}/link/rs{}x{}-{}.cfg".format(self.base_dir, link[0][0], link[0][1], link[0][2]), "w") as file:
-					file.write("{}\n".format(self.__link_param_or_default(link[1][1], "tick_begin",   250000)))
-					file.write("{}\n".format(self.__link_param_or_default(link[1][1], "cycle_min",        40)))
-					file.write("{}\n".format(self.__link_param_or_default(link[1][1], "cycle_max",        64)))
+					file.write("{}\n".format(self.__link_param_or_default(link[1][1], "tick_begin",        0)))
+					file.write("{}\n".format(self.__link_param_or_default(link[1][1], "cycle_min",        50)))
+					file.write("{}\n".format(self.__link_param_or_default(link[1][1], "cycle_max",       100)))
 					file.write("{}\n".format(self.__link_param_or_default(link[1][1], "chance",		      25)))
 					file.write("{}\n".format(self.__link_param_or_default(link[1][1], "filter_app",       -1)))
 					file.write("{}\n".format(self.__link_param_or_default(link[1][1], "filter_prod",      -1)))
