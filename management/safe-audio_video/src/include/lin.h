c8af08ffeb8e852c2b4802070e1aa33cef2fb50b
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2026-01-17 16:22:20 -0300

    Update linear models

--- management/safe-audio_video/src/include/lin.h
+++ management/safe-audio_video/src/include/lin.h
@@ -8,22 +8,22 @@ int lin(const uint16_t rel_time, const uint8_t hops, const uint8_t size, const u
     float prod_weight;
     switch (prod) {
         case 0:
-            prod_weight = 3.99939042e+01;
+            prod_weight = 6.54789631e+01;
             break;
         case 1:
-            prod_weight = 3.03319899e+01;
+            prod_weight = 6.93332892e+00;
             break;
         case 2:
-            prod_weight = -2.51642649e+01;
+            prod_weight = -4.24615707e+01;
             break;
         case 3:
-            prod_weight = -1.42358909e+01;
+            prod_weight = -8.22217962e+00;
             break;
         case 4:
-            prod_weight = -1.33279118e+01;
+            prod_weight = -9.02878776e+00;
             break;
         case 6:
-            prod_weight = -1.75978265e+01;
+            prod_weight = -1.26997540e+01;
             break;
         default:
             prod_weight = 0;
@@ -33,27 +33,27 @@ int lin(const uint16_t rel_time, const uint8_t hops, const uint8_t size, const u
     float cons_weight;
     switch (cons) {
         case 0:
-            cons_weight = 3.03319899e+01;
+            cons_weight = 6.93332892e+00;
             break;
         case 1:
-            cons_weight = -3.01713877e+01;
+            cons_weight = -2.79688229e+01;
             break;
         case 2:
-            cons_weight = -1.42358909e+01;
+            cons_weight = -8.22217962e+00;
             break;
         case 3:
-            cons_weight = -1.33279118e+01;
+            cons_weight = -9.02878776e+00;
             break;
         case 4:
-            cons_weight = 1.25735612e+01;
+            cons_weight = 1.52690688e+01;
             break;
         case 5:
-            cons_weight = 1.48296393e+01;
+            cons_weight = 2.30173925e+01;
             break;
         default:
             cons_weight = 0;
             break;
     }
 
-    return 534.35065605 + -3.16211254e-03 * rel_time + 1.05355063e+01 * hops + 2.50591553e+00 * size + prod_weight + cons_weight;
+    return 528.35186437 + -1.14458313e-03 * rel_time + 7.12896163e+00 * hops + 2.53055636e+00 * size + prod_weight + cons_weight;
 }
