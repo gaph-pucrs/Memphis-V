aba419fb111377c8ec4d1faf34e0ab10ad28b575
Angelo Elias Dal Zotto <angelodalzotto97@gmail.com>
2026-01-17 15:51:27 -0300

    Fix mpeg lin

--- management/safe-mpeg/src/include/lin.h
+++ management/safe-mpeg/src/include/lin.h
@@ -8,16 +8,16 @@ int lin(const uint16_t rel_time, const uint8_t hops, const uint8_t size, const u
     float prod_weight;
     switch (prod) {
         case 0:
-            prod_weight = 1.29798426e+01;
+            prod_weight = 1.36853893e+01;
             break;
         case 1:
-            prod_weight = -6.33168317e+00;
+            prod_weight = -6.71796551e+00;
             break;
         case 2:
-            prod_weight = -6.69224606e+00;
+            prod_weight = -7.01075709e+00;
             break;
         case 4:
-            prod_weight = 4.40866711e-02;
+            prod_weight = 4.33332706e-02;
             break;
         default:
             prod_weight = 0;
@@ -27,21 +27,21 @@ int lin(const uint16_t rel_time, const uint8_t hops, const uint8_t size, const u
     float cons_weight;
     switch (cons) {
         case 0:
-            cons_weight = -6.33168317e+00;
+            cons_weight = -6.71796551e+00;
             break;
         case 1:
-            cons_weight = -6.69224606e+00;
+            cons_weight = -7.01075709e+00;
             break;
         case 2:
-            cons_weight = 4.40866711e-02;
+            cons_weight = 4.33332706e-02;
             break;
-        case 4:
-            cons_weight = 1.29798426e+01;
+        case 3:
+            cons_weight = 1.36853893e+01;
             break;
         default:
             cons_weight = 0;
             break;
     }
 
-    return 495.46310552 + -2.28860036e-03 * rel_time + 6.90666925e+00 * hops + 2.82154695e+00 * size + prod_weight + cons_weight;
+    return 492.97254485 + -2.43337224e-03 * rel_time + 6.79055439e+00 * hops + 2.77332932e+00 * size + prod_weight + cons_weight;
 }
