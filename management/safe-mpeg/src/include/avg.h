392e392d9f277d7950ebccf73e2f85e1b2096d8a
Angelo Elias Dal Zotto <angelodalzotto97@gmail.com>
2026-01-18 16:56:00 -0300

    Add correct average model for MPEG

--- management/safe-mpeg/src/include/avg.h
+++ management/safe-mpeg/src/include/avg.h
@@ -5,14 +5,14 @@
 
 int avg(const uint16_t rel_time, const uint8_t hops, const uint8_t size, const uint8_t prod, const uint8_t cons)
 {
-    if (prod == 4 && cons == 2)
-        return 872.7393353174604;
+    if      (prod == 4 && cons == 2)
+        return 862.5635416666667;
     else if (prod == 2 && cons == 1)
-        return 677.4176339285714;
+        return 669.6031994047619;
     else if (prod == 1 && cons == 0)
-        return 677.8476438492063;
+        return 670.0465773809524;
     else if (prod == 0 && cons == 3)
-        return 716.5191468253968;
+        return 710.7896329365079;
 
-    return 736.1309399801587;
+    return 728.2507378472222;
 }
