b3c48f973bc52d2b20a0df032e74e7a3c0da24f1
Antônio dos Santos <antonio.s001@edu.pucrs.br>
2026-07-14 20:08:02 -0300

    created ARCH variable (was hardcoded)

--- applications/common/common.mk
+++ applications/common/common.mk
@@ -26,8 +26,10 @@ INCMUTILS = $(DIRMUTILS)/src/include
 HDRMUTILS = $(wildcard $(DIRMUTILS)/*.h) $(wildcard $(DIRMUTILS)/**/*.h)
 LIBMUTILS = $(DIRMUTILS)/libmutils.a
 
-CFLAGS	+= -march=rv32imac_zicntr_zicsr_zihpm -mabi=ilp32 -Os -fdata-sections -ffunction-sections -flto -Wall -std=c23 -I$(SRCDIR) -I$(INCMUTILS) -I$(INCMEMPHIS)
-LDFLAGS += -L$(DIRMEMPHIS) -L$(DIRMUTILS) --specs=nano.specs -T $(DIRMEMPHIS)/memphis.ld -Wl,--gc-sections,-flto -u _getpid -march=rv32imac_zicntr_zicsr_zihpm -mabi=ilp32 -lmemphis -lmutils
+ARCH ?= rv32imac_zicntr_zicsr_zihpm
+
+CFLAGS	+= -march=$(ARCH) -mabi=ilp32 -Os -fdata-sections -ffunction-sections -flto -Wall -std=c23 -I$(SRCDIR) -I$(INCMUTILS) -I$(INCMEMPHIS)
+LDFLAGS += -L$(DIRMEMPHIS) -L$(DIRMUTILS) --specs=nano.specs -T $(DIRMEMPHIS)/memphis.ld -Wl,--gc-sections,-flto -u _getpid -march=$(ARCH) -mabi=ilp32 -lmemphis -lmutils
 
 all: $(TARGETS) $(ELFS) $(LSTS)
 
