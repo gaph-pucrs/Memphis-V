71967b6235354a42b068a4ac0d0baa542791f1cf
Antônio dos Santos <antonio.s001@edu.pucrs.br>
2026-07-21 14:43:15 -0300

    revert "added print of total cycles to debug.h" - missed \n
    This reverts commit f3e863520efdb473241c6f574a343ec8a62532fb.

--- applications/lmfrnet/debug.h
+++ applications/lmfrnet/debug.h
@@ -10,12 +10,10 @@ typedef struct {
 #define PRINT_STATS(arr)                                          \
     do {                                                          \
         printf("--- STATS ---\n");                                \
-        unsigned _tot = 0;                                        \
         size_t _len = sizeof(arr) / sizeof((arr)[0]);             \
         for (size_t _i = 0; _i < _len; ++_i) {                    \
             printf("%u\n", (arr)[_i].lapsed);                     \
-            _tot += (arr)[_i].lapsed;                             \
         }                                                         \
-        printf("TOTAL: %u", _tot);                                \
     } while (0)
+
 #endif
