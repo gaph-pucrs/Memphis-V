be9247acf06e32f0957a4f84bc353aae94c0cf33
Antônio dos Santos <antonio.s001@edu.pucrs.br>
2026-07-23 15:34:29 -0300

    fixed _tot was not accumulating the values

--- applications/lmfrnet/debug.h
+++ applications/lmfrnet/debug.h
@@ -14,6 +14,7 @@ typedef struct {
         size_t _len = sizeof(arr) / sizeof((arr)[0]);             \
         for (size_t _i = 0; _i < _len; ++_i) {                    \
             printf("%u\n", (arr)[_i].lapsed);                     \
+            _tot += (arr)[_i].lapsed;                             \
         }                                                         \
         printf("TOTAL: %u\n", _tot);                              \
     } while (0)
